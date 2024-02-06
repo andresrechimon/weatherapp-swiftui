6e6757144aa8600fbf5e98476e277dc3e15257dd
Rechimon, Andrés <andres.r1995@hotmail.com>
2024-02-06 20:08:42 -0300

    Initial Commit

---
+++ swiftui-weather/swiftui_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_weatherApp.swift
+//  swiftui-weather
+//
+//  Created by Andrés Rechimon on 06/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
